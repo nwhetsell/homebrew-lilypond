26fa215c9f71265b9a4ad58907f638e4b8b7a8b6
Nate Whetsell <nathan.whetsell@gmail.com>
2020-08-30 07:36:16 -0400

    Use TeX Gyre fonts included with MacTeX

--- lilypond.rb
+++ lilypond.rb
@@ -1,6 +1,6 @@
 class MacTeXRequirement < Requirement
   fatal true
-  satisfy { Dir.exist?("/Library/TeX/texbin") }
+  satisfy { Dir.exist?("/Library/TeX") }
   def message
     <<~EOS
       MacTeX is required. You can install it by running one of:
@@ -36,21 +36,6 @@ class Lilypond < Formula
   uses_from_macos "flex" => :build
   uses_from_macos "perl" => :build
 
-  resource "font-tex-gyre-cursor" do
-    url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/cursor/qcr2.004otf.zip"
-    sha256 "ae8db1c134ec5c1b8c3999116b0610a5ad1c6f47520c3b4712b9bc914458dce9"
-  end
-
-  resource "font-tex-gyre-heros" do
-    url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/heros/qhv2.004otf.zip"
-    sha256 "755954b7349b803fc1c3d82fe9d9c4f7cf66467af718eaaf4f78ae1a09bf265d"
-  end
-
-  resource "font-tex-gyre-schola" do
-    url "http://www.gust.org.pl/projects/e-foundry/tex-gyre/schola/qcs2.005otf.zip"
-    sha256 "24063368bfdc1046439e290299157621a437294ecbb39a938d2ddb2afa3daaf8"
-  end
-
   resource "font-urw-base35" do
     url "https://github.com/ArtifexSoftware/urw-base35-fonts/archive/20170801.zip"
     sha256 "4505042c8859166f5bff77e33907e244b66eb4e04b56646e14e0a97e5757cd21"
@@ -61,9 +46,6 @@ class Lilypond < Formula
     ENV.append_path "PATH", Formula["guile@1"].bin
     ENV.append_path "PATH", "/Library/TeX/texbin"
 
-    %w[cursor heros schola].each do |suffix|
-      resource("font-tex-gyre-#{suffix}").stage buildpath/"texgyre"
-    end
     resource("font-urw-base35").stage buildpath/"urw"
 
     inreplace "config.make.in",
@@ -73,7 +55,7 @@ class Lilypond < Formula
     mkdir "build" do
       system "../configure", "--disable-documentation",
                              "--prefix=#{prefix}",
-                             "--with-texgyre-dir=#{buildpath}/texgyre",
+                             "--with-texgyre-dir=/Library/TeX/Root/texmf-dist/fonts/opentype/public/tex-gyre",
                              "--with-urwotf-dir=#{buildpath}/urw/fonts"
       system "make"
       system "make", "install"
