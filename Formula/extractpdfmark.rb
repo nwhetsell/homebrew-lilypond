e0e2b3137e68556f222288c2df399f3738a439ce
Nathan Whetsell <nathan.whetsell@gmail.com>
2021-07-11 19:56:10 -0400

    extractpdfmark: revision bump
    Closes #5.
    
    Signed-off-by: Nate Whetsell <nathan.whetsell@gmail.com>

--- Formula/extractpdfmark.rb
+++ Formula/extractpdfmark.rb
@@ -4,6 +4,7 @@ class Extractpdfmark < Formula
   url "https://github.com/trueroad/extractpdfmark/archive/v1.1.0.tar.gz"
   sha256 "edc553051ab49b56cd9120a91b5548a88ee2fb92481330aaf4d45d0e3250761c"
   license "GPL-3.0-or-later"
+  revision 1
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
