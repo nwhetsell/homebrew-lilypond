ccc8f4abf8b3ccefb4e5103d1f9cd95b47b0458d
Nathan Whetsell <nathan.whetsell@gmail.com>
2022-01-09 15:35:14 -0500

    Fix Abjad dependency

--- Formula/abjad.rb
+++ Formula/abjad.rb
@@ -13,7 +13,7 @@ class Abjad < Formula
     sha256 cellar: :any_skip_relocation, catalina: "4e488b3ccb3d50ff5cb3c8dad93897cfa1697c08f09eb51f1245ca512828f83d"
   end
 
-  depends_on "./lilypond"
+  depends_on "lilypond"
   depends_on "python@3.9"
   depends_on "six"
 
