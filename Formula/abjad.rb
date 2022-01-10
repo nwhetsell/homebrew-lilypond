bff59cbc28d4faa2994aa308b55523ffae4b70ac
Nathan Whetsell <nathan.whetsell@gmail.com>
2022-01-10 04:55:15 -0500

    Restore Abjad

---
+++ Formula/abjad.rb
@@ -0,0 +1,96 @@
+class Abjad < Formula
+  include Language::Python::Virtualenv
+
+  desc "Python API for building LilyPond files"
+  homepage "https://abjad.github.io"
+  url "https://files.pythonhosted.org/packages/67/7f/9bfa1bb008820b96105f70e8e9dc0d68e019d14998456711a18f2e3a32df/abjad-3.4.tar.gz"
+  sha256 "c97a21cb420c2332d86b38dc0825b1df34e358033ea0c7384cadd73726a9167a"
+  license "GPL-3.0-only"
+  revision 2
+
+  bottle do
+    root_url "https://github.com/nwhetsell/homebrew-lilypond/releases/download/abjad-3.4_2"
+    sha256 cellar: :any_skip_relocation, catalina: "4e488b3ccb3d50ff5cb3c8dad93897cfa1697c08f09eb51f1245ca512828f83d"
+  end
+
+  depends_on "lilypond"
+  depends_on "python@3.9"
+  depends_on "six"
+
+  resource "Babel" do
+    url "https://files.pythonhosted.org/packages/17/e6/ec9aa6ac3d00c383a5731cc97ed7c619d3996232c977bb8326bcbb6c687e/Babel-2.9.1.tar.gz"
+    sha256 "bc0c176f9f6a994582230df350aa6e05ba2ebe4b3ac317eab29d9be5d2768da0"
+  end
+
+  resource "docutils" do
+    url "https://files.pythonhosted.org/packages/57/b1/b880503681ea1b64df05106fc7e3c4e3801736cf63deffc6fa7fc5404cf5/docutils-0.18.1.tar.gz"
+    sha256 "679987caf361a7539d76e584cbeddc311e3aee937877c87346f31debc63e9d06"
+  end
+
+  resource "imagesize" do
+    url "https://files.pythonhosted.org/packages/f6/27/b147794d43249e8303a06f427e407a090696b65b81045e36f8873d8d8a42/imagesize-1.3.0.tar.gz"
+    sha256 "cd1750d452385ca327479d45b64d9c7729ecf0b3969a58148298c77092261f9d"
+  end
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/91/a5/429efc6246119e1e3fbf562c00187d04e83e54619249eb732bb423efa6c6/Jinja2-3.0.3.tar.gz"
+    sha256 "611bb273cd68f3b993fabdc4064fc858c5b47a973cb5aa7999ec1ba405c87cd7"
+  end
+
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/bf/10/ff66fea6d1788c458663a84d88787bae15d45daa16f6b3ef33322a51fc7e/MarkupSafe-2.0.1.tar.gz"
+    sha256 "594c67807fb16238b30c44bdf74f36c02cdf22d1c8cda91ef8a0ed8dabf5620a"
+  end
+
+  resource "ply" do
+    url "https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz"
+    sha256 "00c7c1aaa88358b9c765b6d3000c6eec0ba42abca5351b095321aef446081da3"
+  end
+
+  resource "Pygments" do
+    url "https://files.pythonhosted.org/packages/94/9c/cb656d06950268155f46d4f6ce25d7ffc51a0da47eadf1b164bbf23b718b/Pygments-2.11.2.tar.gz"
+    sha256 "4e426f72023d88d03b2fa258de560726ce890ff3b630f88c21cbb8b2503b8c6a"
+  end
+
+  resource "pytz" do
+    url "https://files.pythonhosted.org/packages/e3/8e/1cde9d002f48a940b9d9d38820aaf444b229450c0854bdf15305ce4a3d1a/pytz-2021.3.tar.gz"
+    sha256 "acad2d8b20a1af07d4e4c9d2e9285c5ed9104354062f275f3fcd88dcef4f1326"
+  end
+
+  resource "quicktions" do
+    url "https://files.pythonhosted.org/packages/fb/b5/60ed7190463a93e5a21483e42676517814e4d0d30bb4781cfabb3f45fe44/quicktions-1.12.tar.gz"
+    sha256 "5086e39769eb2d4a7f39fe4e70164d713ec851e65dc5078e6c59173826e5aa3c"
+  end
+
+  resource "roman" do
+    url "https://files.pythonhosted.org/packages/49/bc/98ff27d555e8315fc572fb84cb72219322a4ea562ab776687c6fd45335c1/roman-3.3.tar.gz"
+    sha256 "2c46ac8db827d34e4fa9ccc0577e7f0b0d84f16ffe112351bd4f1ec2eb12d73f"
+  end
+
+  resource "Sphinx" do
+    url "https://files.pythonhosted.org/packages/5f/70/83589844bd82a5de3a748757fb39b2440435716e6a295827b13967dfa97f/Sphinx-4.3.2.tar.gz"
+    sha256 "0a8836751a68306b3fe97ecbe44db786f8479c3bf4b80e3a7f5c838657b4698c"
+  end
+
+  resource "Unidecode" do
+    url "https://files.pythonhosted.org/packages/41/a6/93288318cfae2fa0ca978dfe6bb94b22b7e9a9e98b6149a4af00b1e76ee8/Unidecode-1.3.2.tar.gz"
+    sha256 "669898c1528912bcf07f9819dc60df18d057f7528271e31f8ec28cc88ef27504"
+  end
+
+  resource "uqbar" do
+    url "https://files.pythonhosted.org/packages/63/21/3db2314384d664313d2f8bea43b322cf5ea4626ddbf6ddfaed80e1f904c7/uqbar-0.5.8.tar.gz"
+    sha256 "11b3ed16dc36d2fc5fe5958027d510fc96e698a14a15ec7414b954969e2f16af"
+  end
+
+  def install
+    virtualenv_install_with_resources
+
+    (prefix/Language::Python.site_packages("python3")/"homebrew-abjad.pth").write <<~EOS
+      import site; site.addsitedir('#{libexec}')
+    EOS
+  end
+
+  test do
+    system Formula["python@3.9"].bin/"python3", "-c", "import abjad"
+  end
+end
