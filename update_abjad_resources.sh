3103e11d4a436cd3140a31d9b3fbaadb4b4c5b94
Nate Whetsell <nathan.whetsell@gmail.com>
2025-07-31 06:25:57 -0400

    Try to fix shellcheck issues

--- update_abjad_resources.sh
+++ update_abjad_resources.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
 
-brew update-python-resources \
-  --exclude-packages $(echo "
-    alabaster,\
-    black,\
-    pygments,\
-    requests,\
-    roman-numerals-py,\
-    snowballstemmer,\
-    sphinxcontrib-applehelp,\
-    sphinxcontrib-devhelp,\
-    sphinxcontrib-htmlhelp,\
-    sphinxcontrib-jsmath,\
-    sphinxcontrib-qthelp,\
-    sphinxcontrib-serializinghtml" | tr -d '[:blank:]') \
-  abjad
+set -e
+
+excluded_packages="\
+alabaster,\
+black,\
+pygments,\
+requests,\
+roman-numerals-py,\
+snowballstemmer,\
+sphinxcontrib-applehelp,\
+sphinxcontrib-devhelp,\
+sphinxcontrib-htmlhelp,\
+sphinxcontrib-jsmath,\
+sphinxcontrib-qthelp,\
+sphinxcontrib-serializinghtml\
+"
+
+brew update-python-resources --exclude-packages "$excluded_packages" abjad
